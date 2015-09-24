427fc380a2677b0d53658e54af0c362636715f3f
synanhero <synanhero@outlook.com>
2015-09-25 02:44:11 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 sudo apt-get update -y
 sudo apt-get upgrade -y
-sudo apt-get install php5 php-pear -y
 echo 'deb http://packages.dotdeb.org wheezy-php56 all' | sudo tee --append /etc/apt/sources.list > /dev/null
 sudo wget http://www.dotdeb.org/dotdeb.gpg
 sudo apt-key add dotdeb.gpg
@@ -10,14 +9,4 @@ sudo apt-get install php5-cli -y
 sudo touch /var/www/index.php
 sudo chmod 777 /var/www/index.php
 sudo echo "<?php phpinfo();" >> /var/www/index.php
-sudo service apache2 restart
-sudo apt-get install curl libcurl3 libcurl3-dev php5-curl
-cd /usr/local/src
-sudo wget http://curl.haxx.se/download/curl-7.44.0.tar.gz
-sudo tar -xvzf curl-7.44.0.tar.gz
-sudo rm curl-7.44.0.tar.gz
-cd curl-7.44.0
-sudo apt-get install build-essential -y
-sudo ./configure --with-ssl
-sudo make
 sudo service apache2 restart
