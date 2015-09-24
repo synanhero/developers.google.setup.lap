58ef16ddd0702191b40176db999afc8e73369f2c
synanhero <synanhero@outlook.com>
2015-09-24 23:45:42 +0300

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+sudo apt-get update -y
+sudo apt-get upgrade -y
+sudo apt-get install php5 php-pear -y
+sudo echo "deb http://packages.dotdeb.org wheezy-php56 all" >> /etc/apt/sources.list.d/dotdeb.list
+sudo wget http://www.dotdeb.org/dotdeb.gpg
+sudo apt-key add dotdeb.gpg
+sudo apt-get update
+sudo apt-get install php5-cli php5 php-pear -y
+sudo service apache2 restart
+sudo touch /var/www/index.php
+sudo chmod 777 /var/www/index.php
+sudo echo "<?php phpinfo()" >> /var/www/index.php
