d583eb1bc0ab802d29c54cf670df079283f2ae4b
synanhero <synanhero@outlook.com>
2015-09-25 03:55:46 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,7 +3,8 @@ echo 'deb http://packages.dotdeb.org wheezy-php56 all' | sudo tee --append /etc/
 sudo wget http://www.dotdeb.org/dotdeb.gpg
 sudo apt-key add dotdeb.gpg
 sudo apt-get update -y
-sudo apt-get install php5-cli -y
+sudo apt-get upgrade -y
+sudo apt-get install apache2 php5-cli -y
 sudo touch /var/www/index.php
 sudo chmod 777 /var/www/index.php
 sudo echo "<?php phpinfo();" >> /var/www/index.php
